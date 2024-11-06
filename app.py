e7e01afc72ad727d6a42e7340c6bd4f6a79bf7d1
Ygor Mikassio <34070637+ygormikassio@users.noreply.github.com>
2024-11-06 09:00:48 -0300

    Criado funcao da secao de resposta das perguntas do CEO

--- app.py
+++ app.py
@@ -46,3 +46,27 @@ def create_dataframe_section(df):
     col_2.markdown(data_description)
 
     return None
+
+def create_answers_section(df):
+    st.title("Main Questions Answers")
+
+    st.header("First Round")
+    st.subheader("How many bikes are being sold by their owners and how many bikes are being sold by distributors?")
+
+    st.subheader("How many bikes are being sold are bikes from a unique owner?")
+
+    st.subheader("Are high kilometer bikes more expensive than bikes with lower kilometer?")
+
+    st.subheader("Are the bikes with a unique owner more expense on avarege than the other bikes?")
+
+    st.subheader("Are the bikes that have more owners also the bikes with more kilometers traveled on avarege?")
+
+    st.subheader("Which company has the most bikes registered?")
+
+    st.subheader("Which company has the most expensive bikes on avarege?")
+
+    st.subheader("Are the company that has the most expensive bikes registered also the company with the most bikes registered?")
+
+    st.subheader("Which bikes are good for buying?")
+    
+    return None
