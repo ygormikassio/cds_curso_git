8a94c3db2b08a427cdf864b9a094540b60539f37
Ygor Mikassio <34070637+ygormikassio@users.noreply.github.com>
2024-11-25 09:48:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,8 @@ def load_data():
     return pd.read_csv('data/processed/bikes_completed.csv')
 
 def main():
-    df = load_data()
-    
+    df = load_data()    
     create_dataframe_section(df)
-
     create_answers_section(df)
 
     return None
@@ -54,23 +52,14 @@ def create_dataframe_section(df):
 def create_answers_section(df):
     st.title("Main Questions Answers")
     st.header("First Round")
-
     st.subheader("How many bikes are being sold by their owners and how many bikes are being sold by distributors?")
-
     st.subheader("How many bikes are being sold are bikes from a unique owner?")
-
     st.subheader("Are high kilometer bikes more expensive than bikes with lower kilometer?")
-
     st.subheader("Are the bikes with a unique owner more expense on avarege than the other bikes?")
-
     st.subheader("Are the bikes that have more owners also the bikes with more kilometers traveled on avarege?")
-
     st.subheader("Which company has the most bikes registered?")
-
     st.subheader("Which company has the most expensive bikes on avarege?")
-
     st.subheader("Are the company that has the most expensive bikes registered also the company with the most bikes registered?")
-
     st.subheader("Which bikes are good for buying?")
     
     return None
